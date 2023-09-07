96c60cbe17656b2338bca7187f711f81e648515a
inhooo00 <chldlsgh0987@naver.com>
2023-09-07 17:20:34 +0900

    B11052

---
+++ src/main/java/Silver_1/B11052_카드구매하기.java
@@ -0,0 +1,33 @@
+package Silver_1;//예제 입력 1
+//4
+//1 5 6 7
+//예제 출력 1
+//10
+//예제 입력 2
+//5
+//10 9 8 7 6
+//예제 출력 2
+
+import java.util.Scanner;
+
+public class B11052_카드구매하기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n + 1];
+        int[] d = new int[n + 1];
+
+        for (int i = 1; i <= n; i++)
+            arr[i] = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                d[i] = Math.max(d[i], d[i - j] + arr[j]);
+            }
+        }
+
+        System.out.println(d[n]);
+
+
+    }
+}
