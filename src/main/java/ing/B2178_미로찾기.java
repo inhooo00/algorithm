7cc5c7737cd6f73ccad3677fa78b966123e00c07
inhooo00 <chldlsgh0987@naver.com>
2023-09-13 01:03:16 +0900

    main

---
+++ src/main/java/ing/B2178_미로찾기.java
@@ -0,0 +1,20 @@
+package ing;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class B2178_미로찾기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String [ ] arr = br.readLine().split(" ");
+        int N = Integer.parseInt(arr[0]);
+        int m = Integer.parseInt(arr[1]);
+        List<Integer> list = new ArrayList<>();
+        for (int i=0;i<N;i++) list.add(Integer.parseInt(br.readLine()));
+
+
+    }
+}
--- src/main/java/ing/test.java
+++ src/main/java/ing/test.java
@@ -10,22 +10,9 @@ import java.util.*;
 public class test {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        HashMap<Integer, String> map = new HashMap<Integer,String>();
-        int n = Integer.parseInt(br.readLine());
-
-        for(int i = 0; i < n; i++){
-            String input[] = br.readLine().split(" ");
-            int a = Integer.parseInt(input[0]);
-            String b = input[1];
-            map.put(a,b);
-        }
-
-        List<Integer> keyList = new ArrayList<>(map.keySet());
-        keyList.sort((s1, s2) -> s1.compareTo(s2));
-        Set<Integer> keySet = map.keySet();
-
-        for (Integer key : keySet) {
-            System.out.println(key + " " + map.get(key));
+        int N = Integer.parseInt(br.readLine());
+        for (int i=1;i<=N;i+=2){
+            System.out.println('*'*i);
         }
     }
 }
