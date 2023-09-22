e4de57186890ebbe60233c3aa2560a5176076d49
inhooo00 <chldlsgh0987@naver.com>
2023-09-22 23:31:35 +0900

    main

---
+++ src/main/java/ing/B9084_동전.java
@@ -0,0 +1,33 @@
+package ing;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+//예제 입력 1
+//3
+//2
+//1 2
+//1000
+//3
+//1 5 10
+//100
+//2
+//5 7
+//22
+//예제 출력 1
+//501
+//121
+//1
+public class B9084_동전 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i=0;i<T;i++){
+            int N = Integer.parseInt(br.readLine());
+            for (int j=0;j<N;j++){
+                
+            }
+        }
+    }
+}
