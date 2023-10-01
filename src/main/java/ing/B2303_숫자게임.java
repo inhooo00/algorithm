8600ff97bd3bee48b23291ac17dd94ed6352b780
inhooo00 <chldlsgh0987@naver.com>
2023-10-01 16:48:21 +0900

    main

---
+++ src/main/java/ing/B2303_숫자게임.java
@@ -0,0 +1,20 @@
+package ing;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class B2303_숫자게임 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N =Integer.parseInt(br.readLine()); //사람 수
+        List<Integer> list = new ArrayList<>();
+        for (int i=1;i<=N;i++){
+            String[] input = br.readLine().split(" ");
+           // for (int j=0;j<N;j++) //list.add(j) =Integer.parseInt(input[j]);
+        }
+
+    }
+}
