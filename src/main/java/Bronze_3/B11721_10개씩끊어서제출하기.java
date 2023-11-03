20b0719f4ae32ea82d6e375b1e1dad3518b7aac5
inhooo00 <chldlsgh0987@naver.com>
2023-11-03 21:18:40 +0900

    main

---
+++ src/main/java/Bronze_3/B11721_10개씩끊어서제출하기.java
@@ -0,0 +1,18 @@
+package Bronze_3;
+
+import java.util.Scanner;
+
+public class B11721_10개씩끊어서제출하기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        sc.close();
+
+        for (int i = 0; i < input.length(); ++i) {
+            System.out.print(input.charAt(i));
+            if (i % 10 == 9) {
+                System.out.println();
+            }
+        }
+    }
+}
