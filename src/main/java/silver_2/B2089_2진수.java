05c807b10f2b0c95279a56b7b37c5e0cb75e4a1b
inhooo00 <chldlsgh0987@naver.com>
2023-12-04 11:46:40 +0900

    main

---
+++ src/main/java/silver_2/B2089_2진수.java
@@ -0,0 +1,25 @@
+package silver_2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class B2089_2진수 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        List<Integer> list = new ArrayList<>();
+
+        if(n==0) //n이 0 일때
+            System.out.println(n);
+
+        while(n!=0) {
+            list.add(Math.abs(n%-2));
+            n=(int)Math.ceil((double)n/-2);
+        }
+
+        for(int i=list.size()-1; i>=0; i--)
+            System.out.print(list.get(i));
+    }
+
+}
