a76e3f60e12578b8309936b4e1e0379e34192cd4
inhooo00 <chldlsgh0987@naver.com>
2024-01-07 01:40:07 +0900

    스택 큐

---
+++ src/main/java/programmers/level2/P12909.java
@@ -0,0 +1,38 @@
+package programmers.level2;
+
+//s	answer
+//"()()"	true
+//"(())()"	true
+//")()("	false
+//"(()("	false
+public class P12909 {
+    public static void main(String[] args) {
+        System.out.println(solution("()()"));
+    }
+
+        public static boolean solution(String s) {
+            int openCount = 0;  // 여는 괄호 개수
+            int closeCount = 0; // 닫는 괄호 개수
+
+            if (s.charAt(0) == '(' && s.charAt(s.length() - 1) == ')') {
+                for (char c : s.toCharArray()) {
+                    if (c == '(') {
+                        openCount++;
+                    } else if (c == ')') {
+                        closeCount++;
+                    }
+
+                    // 여는 괄호 개수보다 닫는 괄호 개수가 더 많으면 올바르지 않은 괄호 문자열
+                    if (closeCount > openCount) {
+                        return false;
+                    }
+                }
+
+                // 여는 괄호와 닫는 괄호 개수가 동일하면 올바른 괄호 문자열
+                return openCount == closeCount;
+            }
+
+            return false;
+        }
+
+}
