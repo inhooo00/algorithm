7acac1ecd8e381d35c9881b7af64f52793aa9235
inhooo00 <chldlsgh0987@naver.com>
2024-01-18 15:11:32 +0900

    main

---
+++ src/main/java/programmers/greedy/level2/P42860.java
@@ -0,0 +1,14 @@
+package programmers.greedy.level2;
+// 조이스틱
+// name	return
+//"JEROEN"	56
+//"JAN"	23
+public class P42860 {
+    public static void main(String[] args) {
+
+    }
+    public static int solution(String name) {
+        int answer = 0;
+        return answer;
+    }
+}
