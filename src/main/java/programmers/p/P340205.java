a455e5dda171b53610ab066529b0b3a6d48e274a
inhooo00 <chldlsgh0987@naver.com>
2024-12-27 14:19:38 +0900

    P340205

---
+++ src/main/java/programmers/p/P340205.java
@@ -0,0 +1,22 @@
+package programmers.p;
+
+import java.util.Scanner;
+
+public class P340205 {
+
+
+    public class Solution {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int number = sc.nextInt();
+            int answer = 0;
+
+            while (number > 0) {
+                answer += number % 100;
+                number /= 100;
+            }
+
+            System.out.println(answer);
+        }
+    }
+}
