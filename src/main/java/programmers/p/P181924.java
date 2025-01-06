65271bbbcc8cee2693c2944525a75701a3530f9b
inhooo00 <chldlsgh0987@naver.com>
2025-01-06 23:58:04 +0900

    P181924

--- src/main/java/programmers/p/P181924.java
+++ src/main/java/programmers/p/P181924.java
@@ -1,22 +1,22 @@
-package programmers.p;
-
-import java.util.Arrays;
+//package programmers.p;
 //
-// 수열 구간 쿼리 3
-//arr	queries	result
-//[0, 1, 2, 3, 4]	[[0, 3],[1, 2],[1, 4]]	[3, 4, 1, 0, 2]
-public class P181924 {
-    public static void main(String[] args) {
-
-    }
-   class Solution {
-        public int[] solution(int[] arr, int[][] queries) {
-            for(int[] query : queries) {
-                int temp = arr[query[0]];
-                arr[query[0]] = arr[query[1]];
-                arr[query[1]] = temp;
-            }
-            return arr;
-        }
-    }
-    }
+//import java.util.Arrays;
+////
+//// 수열 구간 쿼리 3
+////arr	queries	result
+////[0, 1, 2, 3, 4]	[[0, 3],[1, 2],[1, 4]]	[3, 4, 1, 0, 2]
+////public class P181924 {
+////    public static void main(String[] args) {
+////
+////    }
+//   class Solution {
+//        public int[] solution(int[] arr, int[][] queries) {
+//            for(int[] query : queries) {
+//                int temp = arr[query[0]];
+//                arr[query[0]] = arr[query[1]];
+//                arr[query[1]] = temp;
+//            }
+//            return arr;
+//        }
+//    }
+//    }
