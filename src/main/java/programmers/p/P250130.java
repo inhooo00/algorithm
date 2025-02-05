df8b77c9855b336a43632ac269f1eb9e151e1622
inhooo00 <chldlsgh0987@naver.com>
2025-02-05 19:39:14 +0900

    P181875

--- src/main/java/programmers/p/P250130.java
+++ src/main/java/programmers/p/P250130.java
@@ -1,28 +1,28 @@
-package programmers.p;
-
-import java.util.Scanner;
-
-public class P250130 {
-
-    public class Solution {
-        public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
-            int start = sc.nextInt();
-            int before = sc.nextInt();
-            int after = sc.nextInt();
-
-            int money = start;
-            int month = 1;
-            while (money < 70) {
-                money += before;
-                month++;
-            }
-            while (money < 100) {
-                money += after;
-                month++;
-            }
-
-            System.out.println(month);
-        }
-    }
-}
+//package programmers.p;
+//
+//import java.util.Scanner;
+//
+//public class P250130 {
+//
+//    public class Solution {
+//        public static void main(String[] args) {
+//            Scanner sc = new Scanner(System.in);
+//            int start = sc.nextInt();
+//            int before = sc.nextInt();
+//            int after = sc.nextInt();
+//
+//            int money = start;
+//            int month = 1;
+//            while (money < 70) {
+//                money += before;
+//                month++;
+//            }
+//            while (money < 100) {
+//                money += after;
+//                month++;
+//            }
+//
+//            System.out.println(month);
+//        }
+//    }
+//}
