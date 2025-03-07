2f801fd4263b4c66c87cf6dec271c1576f39d5bb
inhooo00 <chldlsgh0987@naver.com>
2025-03-07 14:01:11 +0900

    P17682

--- src/main/java/programmers/p/P388352.java
+++ src/main/java/programmers/p/P388352.java
@@ -1,4 +1,20 @@
 package programmers.p;
 
+// 보류..
+
 public class P388352 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int n = 10;
+        int[][] q = {{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}, {3, 7, 8, 9, 10}, {2, 5, 7, 9, 10}, {3, 4, 5, 6, 7}};
+        int[] ans = {2, 3, 4, 3, 3};
+        System.out.println(solution.solution(n, q, ans));
+    }
+
+    static class Solution {
+        public int solution(int n, int[][] q, int[] ans) {
+            int answer = 0;
+            return answer;
+        }
+    }
 }
