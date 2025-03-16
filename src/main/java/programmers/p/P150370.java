696e818c6957c0ecbcc0e19855e2c757858ea6b2
inhooo00 <chldlsgh0987@naver.com>
2025-03-17 02:43:06 +0900

    P150370

--- src/main/java/programmers/p/P150370.java
+++ src/main/java/programmers/p/P150370.java
@@ -19,9 +19,9 @@ public class P150370 {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        String today = "2020.01.01";
-        String[] terms = {"Z 3", "D 5"};
-        String[] privacies = {"2019.01.01 D", "2019.11.15 Z", "2019.08.02 D", "2019.07.01 D", "2018.12.28 Z"};
+        String today = "2022.05.19";
+        String[] terms = {"A 6", "B 12", "C 3"};
+        String[] privacies = {"2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"};
         int[] answer = solution.solution(today, terms, privacies);
         for (int i : answer) {
             System.out.println(i);
@@ -43,21 +43,15 @@ public class P150370 {
                 mapTerms.put(string[0], Integer.parseInt(string[1]) * 28);
             }
 
-            Map<Integer, String> mapPrivacies = new HashMap<>();
             for (int i = 0; i < privacies.length; i++) {
                 String[] string = privacies[i].split(" ");
                 String[] integers = string[0].split("\\.");
                 int sum =
                         Integer.parseInt(integers[0]) * 12 * 28 + Integer.parseInt(integers[1]) * 28 + Integer.parseInt(
                                 integers[2]);
-                mapPrivacies.put(sum, string[1]);
-            }
 
-            int i = 0;
-            for (Integer key : mapPrivacies.keySet()) {
-                i++;
-                if (key + mapTerms.get(mapPrivacies.get(key)) > todaySum) {
-                    answer.add(i);
+                if (sum + mapTerms.get(string[1]) <= todaySum) {
+                    answer.add(i + 1);
                 }
             }
 
