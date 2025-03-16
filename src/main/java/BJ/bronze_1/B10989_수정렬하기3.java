f84f2e1642274277457fa10acb278f3312e3bc49
inhooo00 <chldlsgh0987@naver.com>
2025-03-17 02:24:56 +0900

    P150370 맨 처음 구성

--- src/main/java/BJ/bronze_1/B10989_수정렬하기3.java
+++ src/main/java/BJ/bronze_1/B10989_수정렬하기3.java
@@ -1,9 +1,12 @@
 package BJ.bronze_1;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Arrays;
+import java.util.TreeMap;
 
 public class B10989_수정렬하기3 {
     public static void main(String[] args) throws IOException {
