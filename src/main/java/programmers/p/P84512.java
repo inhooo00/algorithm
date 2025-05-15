2de79447380b2b99ca978a0e90f06599a6239d12
inhooo00 <chldlsgh0987@naver.com>
2025-05-16 02:59:24 +0900

    P84512

---
+++ src/main/java/programmers/p/P84512.java
@@ -0,0 +1,35 @@
+package programmers.p;
+
+import java.util.Collections;
+import java.util.List;
+
+public class P84512 {
+
+    class Solution {
+        public int solution(String word) {
+            List<String> list = new ArrayList<>();
+            String[] arr = {"A", "E", "I", "O", "U"};
+
+            for (int i = 0; i < 5; i++) {
+                list.add(arr[i]);
+                for (int j = 0; j < 5; j++) {
+                    list.add(arr[i] + arr[j]);
+                    for (int k = 0; k < 5; k++) {
+                        list.add(arr[i] + arr[j] + arr[k]);
+                        for (int l = 0; l < 5; l++) {
+                            list.add(arr[i] + arr[j] + arr[k] + arr[l]);
+                            for (int m = 0; m < 5; m++) {
+                                list.add(arr[i] + arr[j] + arr[k] + arr[l] + arr[m]);
+                            }
+                        }
+                    }
+                }
+            }
+
+            Collections.sort(list);
+
+            return list.indexOf(word) + 1;
+        }
+    }
+
+}
