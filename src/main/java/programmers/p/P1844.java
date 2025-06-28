5b4a68d4822a2b4164acae15b2b8c4c35425dbad
inhooo00 <chldlsgh0987@naver.com>
2025-06-28 16:09:24 +0900

    m1844 다시 풀어보기

--- src/main/java/programmers/p/P1844.java
+++ src/main/java/programmers/p/P1844.java
@@ -21,38 +21,43 @@ public class P1844 {
     }
 
     static class Solution {
-        private static final int[] dx = {0, 0, -1, 1};
-        private static final int[] dy = {-1, 1, 0, 0};
+
+        int[][] directions = {
+                {-1, 0}, // 상 (x-1)
+                {1, 0},  // 하 (x+1)
+                {0, -1}, // 좌 (y-1)
+                {0, 1}   // 우 (y+1)
+        };
 
         public int solution(int[][] maps) {
-            int n = maps.length;
-            int m = maps[0].length;
-            boolean[][] visited = new boolean[n][m];
-            StringBuilder sb = new StringBuilder();
-            Deque<int[]> deque = new LinkedList<>();
-            deque.add(new int[]{0, 0, 1});
+            int a = maps.length;
+            int b = maps[0].length;
+            Deque<int[]> deque = new LinkedList();
+            boolean[][] visited = new boolean[a][b];
+            deque.add(new int[]{0,0,1});
             visited[0][0] = true;
 
             while (!deque.isEmpty()) {
-                int[] current = deque.poll();
-                int x = current[0];
-                int y = current[1];
-                int cnt = current[2];
+                int[] now = deque.poll();
+                int x = now[0];
+                int y = now[1];
+                int cnt = now[2];
 
-                if (x == n - 1 && y == m - 1) {
+                if (x== a-1 && y== b-1) {
                     return cnt;
                 }
-                for (int i = 0; i < 4; i++) {
-                    int nx = x + dx[i];
-                    int ny = y + dy[i];
 
-                    if (nx >= 0 && ny >= 0 && nx < n && ny < m && maps[nx][ny] == 1 && !visited[nx][ny]) {
+                for (int i = 0; i < 4; i++) {
+                    int nx = x + directions[i][0];
+                    int ny = y + directions[i][1];
+
+                    if (nx>=0 && ny >=0 && !visited[nx][ny] && maps[nx][ny] ==1){
                         visited[nx][ny] = true;
-                        deque.add(new int[]{nx, ny, cnt + 1});
+                        deque.add(new int[] {nx, ny, cnt +1});
                     }
+
                 }
             }
-
             return -1;
         }
     }
