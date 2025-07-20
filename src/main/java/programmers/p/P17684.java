2f70da21859019a0e341f44ce9b159c68aea2166
inhooo00 <chldlsgh0987@naver.com>
2025-07-20 21:42:31 +0900

    P12913

--- src/main/java/programmers/p/P17684.java
+++ src/main/java/programmers/p/P17684.java
@@ -9,7 +9,7 @@ public class P17684 {
     public static void main(String[] args) {
 
         Solution solution = new Solution();
-        String msg = "KAKAO";
+        String msg = "TOBEORNOTTOBEORTOBEORNOT";
         List<Integer> result = solution.solution(msg);
         for (int i : result) {
             System.out.print(i + " ");
@@ -18,28 +18,34 @@ public class P17684 {
 
     static class Solution {
         public List<Integer> solution(String msg) {
-            List<Integer> answer = new ArrayList<>();
-            Map <String, Integer> map = new HashMap<>();
+            List<Integer> list = new ArrayList<>();
+            Map<String, Integer> map = new HashMap<>();
             int index = 1;
-            for(char c = 'A'; c <= 'Z'; c++)
-                map.put(String.valueOf(c), index++);
-
+            for (char i = 'A'; i <= 'Z'; i++) {
+                map.put(String.valueOf(i), index++);
+            }
             int len = 0;
             for (int i = 0; i < msg.length(); i = len - 1) {
                 boolean check = false;
-                for (int j = i+1; j < msg.length(); j++) {
+                for (int j = i + 1; j <= msg.length(); j++) {
                     len = j;
-                    if (map.get(msg.substring(i,j))== null) {
+                    if (map.get(msg.substring(i, j)) == null) {
                         check = true;
                         break;
                     }
                 }
-                if (!check) answer.add(map.get(msg.substring(i,len)));
-                else answer.add(map.get((msg.substring(i, len - 1))));
-                map.put(msg.substring(i,len),index++);
-
+                if (check) { // 맵에 값이 없어
+                    list.add(map.get(msg.substring(i, len - 1)));
+                } else // 끝까지 맵에 값이 있으면
+                {
+                    list.add(map.get(msg.substring(i, len)));
+                }
+                map.put(msg.substring(i, len), index++);
+                if (!check && len == msg.length()) {
+                    break;
+                }
             }
-            return answer;
+            return list;
         }
     }
 }
