93c267fe49f856a3263b849284d9e969eb7ddf39
inhooo00 <chldlsgh0987@naver.com>
2025-11-19 15:58:35 +0900

    계단오르기, 2*N 타일링

---
+++ src/main/java/BJ/silver_3/B11726_2N타일링2.java
@@ -0,0 +1,24 @@
+package BJ.silver_3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B11726_2N타일링2 {
+    static int N;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        long[] dp = new long[N+1];
+
+        dp[1] =1;
+        if (N>=2){
+            dp[2] = 3;
+        }
+        for (int i = 3; i <= N; i++) {
+            dp[i] = (dp[i-1] + dp[i-2]*2)%10007;
+        }
+        System.out.println(dp[N]);
+    }
+}
