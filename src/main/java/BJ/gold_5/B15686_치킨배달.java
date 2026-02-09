0defd2e999e633941d8aa5363a487b36b8e72780
inhooo00 <chldlsgh0987@naver.com>
2026-02-09 15:12:13 +0900

    B15686_치킨배달

--- src/main/java/BJ/gold_5/B15686_치킨배달.java
+++ src/main/java/BJ/gold_5/B15686_치킨배달.java
@@ -10,79 +10,100 @@ import java.util.Deque;
 import java.util.List;
 import javax.swing.plaf.SpinnerUI;
 
-// 빈칸 0, 집 1, 치킨 2
-// 치킨거리는 멘헤튼 거리 (1,2) -> (2,1) = 2
-// 가장 많은 수입을 낼 수 있는 치킨집 개수 구하기.
-// N과 M 입력.
 public class B15686_치킨배달 {
-    static int N, M;
+    static int  N,M;
     static int[][] field;
-    static boolean[] open;
-    static List<Node> chickenList = new ArrayList<>();
-    static List<Node> homeList = new ArrayList<>();
-    static int result = Integer.MAX_VALUE;
-
+    static List<Node> home = new ArrayList<>();
+    static List<Node> ck = new ArrayList<>();
+    static int answer = Integer.MAX_VALUE;
+    static boolean[][] visited;
+    static int[][] rows = {
+            {0,1},
+            {0,-1},
+            {1,0},
+            {-1,0}
+    };
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] input = br.readLine().split(" ");
         N = Integer.parseInt(input[0]);
-        M = Integer.parseInt(input[1]);
-
+        M = Integer.parseInt(input[1]); // 최대 치킨 가게
         field = new int[N][N];
-        for (int i = 0; i < N; i++) {
+        visited = new boolean[N][N];
+        for(int i=0;i<N;i++){
             String[] input2 = br.readLine().split(" ");
-            for (int j = 0; j < N; j++) {
+            for(int j=0;j<N;j++){
                 field[i][j] = Integer.parseInt(input2[j]);
-                if (field[i][j] == 2) {
-                    chickenList.add(new Node(i, j));
-                }else if (field[i][j] == 1){
-                    homeList.add(new Node(i,j));
-                }
+                if (field[i][j]==1) home.add(new Node(i,j));
+                else if(field[i][j]==2) ck.add(new Node(i,j));
             }
         }
-        // 최대 M개 고를 수 있고, 도시의 치킨 거리의 최소값을 구해야함.
-        // 1이 2로 도착해야하는데, 일단 2들 중 M개를 골라야 하니까
-        // 조합론 써야할 듯.
-        // 그렇게 계산하고 나온 거리를 기준으로 배열마다의 멘해튼 거리를 구하고 합을 구한다? 일 듯.
-        open = new boolean[chickenList.size()];
-        dfs(0, 0);
-        System.out.println(result);
-    }
 
-    private static void dfs(int size, int start) {
-        if (size == M) {
-            result=Math.min(result,cal()); // M개의 치킨리스트만 남으면 각각 치킨거리 계산하고 총합 저장.
+        // dfs로 M개의 치킨비을 고르는 조합을 구하고 멘해튼 거리 갱신해서 최대가 나오도록.
+        dfs(0,0); // start, depth(현재 깊이)
+        System.out.println(answer);
+    }
+    static void dfs(int start, int depth){
+        if (depth==M){
+            int calNum = cal();
+            answer = Math.min(answer, calNum);
             return;
         }
-        for (int i = start; i < chickenList.size(); i++) {
-            open[i] =true;
-            dfs(size + 1, i + 1);
-            open[i] = false;
+        for(int i=start;i<ck.size();i++){
+            Node nowCK = ck.get(i);
+            if (!visited[nowCK.x][nowCK.y]){
+                visited[nowCK.x][nowCK.y] = true;
+                dfs(i+1,depth+1);
+                visited[nowCK.x][nowCK.y] = false;
+            }
+
         }
     }
+    // 현재 상황에서 모든 집을 bfs 돌렸을 때 합을 리턴하자.
+    static int cal(){
+        int rt = 0;
 
-    private static int cal() {
-        int num = 0;
-        for (Node node : homeList){ // 집 리스트들
-            int total = Integer.MAX_VALUE;
-            for (int i = 0; i < chickenList.size(); i++) { // 치킨 리스트들
-                if (open[i]){ // 치킨집 열려있으면 거리 구하는데 가장 짧은 값만 취함.
-                    Node c = chickenList.get(i);
-                    int dist = Math.abs(c.x-node.x)+Math.abs(c.y-node.y);
-                    total = Math.min(total,dist);
+        for(Node n : home){ // 집 리스트 순회
+            boolean[][] homeVisited = new boolean[N][N]; // 집이 들린 곳은 안 들리게
+            Deque<Node> deque = new ArrayDeque<>();
+            deque.add(n);
+            homeVisited[n.x][n.y] = true;
+            int targetX = 0;
+            int targetY = 0;
+            int count = 0; // 지금 검사하는 집에 대한 카운트
+            // 해당 집에 대해서 bfs를 통해 가장 가까운 치킨집을 찾자.
+            while(!deque.isEmpty()){
+                boolean check = false; // 도착여부
+                Node now = deque.remove();
+
+                for(int[] r : rows){
+                    int newX = r[0] + now.x;
+                    int newY = r[1] + now.y;
+                    if(newX < 0 || newX >= N || newY < 0 || newY >= N) continue;
+                    if (!homeVisited[newX][newY]){
+                        if (visited[newX][newY]) {
+                            check = true;
+                            targetX = newX;
+                            targetY = newY;
+                            break;
+                        }
+                        homeVisited[newX][newY] = true;
+                        deque.add(new Node(newX, newY));
+                    }
+                }
+                if (check) {
+                    break;
                 }
             }
-            num+=total;
+
+            rt +=Math.abs(targetX-n.x) + Math.abs(targetY-n.y);
         }
-
-        return num;
-
+        return rt;
     }
 
-    static class Node {
-        int x, y;
-
-        Node(int x, int y) {
+    static class Node{
+        int x,y;
+        Node(int x, int y){
             this.x = x;
             this.y = y;
         }
