ead89fb12d60f5a07644f07fb325db6ec09b1f2b
inhooo00 <chldlsgh0987@naver.com>
2026-02-11 13:44:50 +0900

    S1486_장훈이의높은선반

---
+++ src/main/java/swea/d4/S1486_장훈이의높은선반.java
@@ -0,0 +1,50 @@
+package swea.d4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+// 조합
+public class S1486_장훈이의높은선반 {
+    static int B, N;
+    static int[] people;
+    static int min;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= T; i++) {
+            int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            N = input[0]; // 점원 수
+            B = input[1]; // 선반 높이
+            min = Integer.MAX_VALUE;
+            people = new int[N];
+            visited = new boolean[N];
+            String[] input2 = br.readLine().split(" ");
+            for (int j = 0; j < N; j++) {
+                people[j] = Integer.parseInt(input2[j]);
+            }
+
+            dfs(0, 0);
+            System.out.println("#" + i + " " + (min-B));
+        }
+    }
+
+    private static void dfs(int depth, int total) {
+        if (total >= B) {
+            min = Math.min(min, total);
+        }
+
+        if (depth == N) {
+            return;
+        }
+
+        dfs(depth + 1, total + people[depth]);
+
+        dfs(depth + 1, total);
+    }
+}
+
